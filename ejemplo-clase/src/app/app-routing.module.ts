7cb666f03b29f34b273b5ae6f680690b03c2f9f5
Andres Arias <aarias.ucr@gmail.com>
2020-05-11 20:30:57 -0600

    Arreglos

--- ejemplo-clase/src/app/app-routing.module.ts
+++ ejemplo-clase/src/app/app-routing.module.ts
@@ -9,7 +9,7 @@ const routes: Routes = [
   {path: '', component: HomeComponent, canActivate: [RouteGuard]},
   {path: 'home', component: HomeComponent, canActivate: [RouteGuard]},
   {path: 'author/:authorName', component: AuthorComponent, canActivate: [RouteGuard]},
-  {path: 'login', component: LoginComponentasdasldkhasjkdhaskjdb}
+  {path: 'login', component: LoginComponent}
 ];
 
 @NgModule({
