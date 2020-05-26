4deaa337f93401528040f1103801eb296e51c2a3
Andres Arias <aarias.ucr@gmail.com>
2020-05-25 20:31:01 -0600

    Desconectar de ConfigCat

--- ejemplo-clase/src/app/shared/config.service.ts
+++ ejemplo-clase/src/app/shared/config.service.ts
@@ -1,17 +1,18 @@
 import {Injectable} from '@angular/core';
-import * as configcat from 'configcat-js';
-import {IConfigCatClient} from 'configcat-common/lib/ConfigCatClient';
+// import * as configcat from 'configcat-js';
+// import {IConfigCatClient} from 'configcat-common/lib/ConfigCatClient';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ConfigService {
-  configCatClient: IConfigCatClient;
+  // configCatClient: IConfigCatClient;
   constructor() {
-    this.configCatClient = configcat.createClient('GQHYCH9da06FNn626m4-cA/Dtz4WPhDy0O2Beo_ST7cHg');
+    // this.configCatClient = configcat.createClient('GQHYCH9da06FNn626m4-cA/Dtz4WPhDy0O2Beo_ST7cHg');
   }
 
   async getConfig(featureName: string) {
-    return this.configCatClient.getValueAsync(featureName, false);
+    // return this.configCatClient.getValueAsync(featureName, false);
+    return Promise.resolve(false);
   }
 }
