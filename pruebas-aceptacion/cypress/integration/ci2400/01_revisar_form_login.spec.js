3c8869c9beef8685d11016da282b48c9c55696f0
Andres Arias <aarias.ucr@gmail.com>
2020-05-28 20:38:41 -0600

    Version inicial de Cypress

---
+++ pruebas-aceptacion/cypress/integration/ci2400/01_revisar_form_login.spec.js
@@ -0,0 +1,38 @@
+/// <reference types="Cypress" />
+
+context('Formulario de Login', () => {
+  beforeEach(() => {
+    cy.visit('https://ejemplo-ci2400.firebaseapp.com/')
+  })
+
+  it('Revisar validaciones formulario login', () => {
+    cy.title().should('eq', 'EjemploClase')
+
+    cy.get('#email').should('be.visible')
+    cy.get('#email').should('be.enabled')
+
+    cy.get('#password').should('be.visible')
+    cy.get('#password').should('be.enabled')
+
+    cy.get('button.botonbonito').should('be.visible')
+    cy.get('button.botonbonito').should('not.be.enabled')
+
+    // cy.get('#email').type('patito@patito.com')
+    cy.get('#email').type('incorrecto@incorrecto.com')
+    cy.get('button.botonbonito').should('not.be.enabled')
+
+    cy.get('#password').type('123456')
+
+    // El boton se habilita hasta que el formulatio
+    // sea valido
+    cy.get('button.botonbonito').should('be.enabled')
+
+    cy.get('button.botonbonito').click()
+
+    // Mensaje de error
+    cy.get('.toast-message').should('be.visible')
+    cy.get('.toast-title').should('be.visible')
+    cy.get('.toast-title').should('contain.text', 'Error iniciando sesión')
+    cy.get('.toast-message').should('contain.text', 'There is no user record')
+  })
+})
