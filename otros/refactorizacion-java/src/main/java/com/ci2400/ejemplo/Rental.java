d99dec8e19e7f1ef688fb694e0f4dca48172a8f6
Andres Arias <aarias.ucr@gmail.com>
2020-06-11 15:50:58 -0600

    Version inicial refactoring

---
+++ otros/refactorizacion-java/src/main/java/com/ci2400/ejemplo/Rental.java
@@ -0,0 +1,21 @@
+package com.ci2400.ejemplo;
+
+
+public class Rental
+{
+  public Rental (Movie movie, int daysRented) {
+    this.movie 		= movie;
+    this.daysRented = daysRented;
+  }
+
+  public int getDaysRented () {
+    return daysRented;
+  }
+
+  public Movie getMovie () {
+    return movie;
+  }
+
+  private Movie movie;
+  private int daysRented;
+}
